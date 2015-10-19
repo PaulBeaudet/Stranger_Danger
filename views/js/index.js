06a60014b92eff09b6134272c6b2ddbd67b8b1ad
paulBeaudet <inof8or@gmail.com>
2015-10-19 11:28:44 -0400

    Change send button dialog on mode change, clarify some flow in comments

--- views/js/index.js
+++ views/js/index.js
@@ -2,24 +2,9 @@
 // Constants, these can change the behavior of the application
 var WAIT_TIME = 30;  // time to wait for talking turn
 var NUM_ENTRIES = 6; // number of dialog rows allowed in the application
-var SERVER = 'http://192.168.1.133:3000';
 var NUM_TIMERS = NUM_ENTRIES + 1; // timer 6 is for the send button
 // call NUM_ENTRIES for send button timer
-var WAIT_MSG = "Waiting for topics...";
-var BTN_SENDING = "done";
-var BTN_STARTING = "Make topic";
 var SEND_TIMER = NUM_ENTRIES;
-// Typing modes
-var BREAK = 0;
-var CHAT = 1;
-var BLOCK = 2;
-// info messages
-var INFO_HOME = "Pick a topic, create topic or get matched";
-var INFO_CHAT = "Chat: Pressing done while text empty, finishes chat";
-var INFO_TIMEOUT = "Actions timed, auto-complete on timeout";
-var INFO_REJECTION = "This word will reject you from the conversation";
-var INFO_FILTER = "Words can be auto rejected by preferance with a filter";
-
 
 // Methods revolving around tracking possition of row
 var edit = { // dep: rows, time, textBar, edit
@@ -43,8 +28,8 @@ var edit = { // dep: rows, time, textBar, edit
     },
     myTurn: function(){
         send.mode = CHAT;             // allow user to type
+        textBar.changeAction(CHAT);   // clear text box and use correct dialog
         time.from(edit.row, "other"); // write other onto the last row
-        textBar.entry.value = "";     // show user that they can type now
         edit.increment();             // increment place to write to
         time.countDown(SEND_TIMER, send.passOn);     // time out input
     },
@@ -61,38 +46,42 @@ var edit = { // dep: rows, time, textBar, edit
 // transition handling of visual elements
 var trans = { // dep: send, time, rows, textBar
     selBreak: function(){  // respond to someone else's topic
-        send.mode = CHAT; // signal to the sender that is it now time to chat
         var row = this.id[this.id.length-1];
         var user = topic.user[parseInt(row)];
         send.to = user;
         sock.et.emit("selBreak", user); // signal which user that needs to be connected with
+        send.mode = CHAT; // signal to the sender that is it now time to chat
         trans.ition({perspec: "other", head: rows.dialog[row].innerHTML});
         time.countDown(SEND_TIMER, send.passOn); // set stopwatch for sending a message
     },
     gotBreak: function(user){         // someone responded to users personal
-        send.mode = BLOCK;            // signal user is listening to someones response
         send.to = user;               // keep track of who we are talking to
         time.stopSend("");            // stop time to live timer
+        send.mode = BLOCK;            // signal user is listening to someones response
         trans.ition({perspec: "you", head: document.getElementById("textEntry").value});
     },
     ition: function(opener){
         time.clear();
         rows.reset();
         if(opener){
-            time.from(0, opener.perspec);              // write perspective to first row
-            rows.dialog[0].innerHTML = opener.head;    // write topic to top of page
-            textBar.btnTxt.innerHTML = BTN_SENDING;// set the instruction on the send button
-            info.chat();
-            edit.row = 1;
-        } else { // transition back to home view
-            textBar.btnTxt.innerHTML = BTN_STARTING;
-            info.home();
-            edit.row = 0;
-            topic.user = []; // clear users from topic list
+            time.from(0, opener.perspec);           // write perspective to first row
+            rows.dialog[0].innerHTML = opener.head; // write topic to top of page
+            info.chat();       // set hint text for chat mode
+            edit.row = 1;      // new dialog starts at possition 1
+        } else {               // transition back to topic making and picking mode
+            send.mode = BREAK; // reset into topic mode
+            info.home();       // Set hint text for topic picking mode
+            edit.row = 0;      // topic can now posted to possition 0
+            topic.user = [];   // clear users from old topic list if exist
         }
-        textBar.entry.value = "";
+        textBar.changeAction(send.mode); // make sure send mode is properly set before trans.ition called
     }
 }
+
+// Typing modes
+var BREAK = 0;
+var CHAT = 1;
+var BLOCK = 2;
 
 // sending logic
 var send = { // dep: sock, trans, edit, textBar
@@ -108,13 +97,13 @@ var send = { // dep: sock, trans, edit, textBar
         } else if (send.mode === CHAT){
             if(send.empty){
                 sock.et.emit('endChat', send.to);
-                trans.ition(); // trasition back to home screen
-                send.mode = BREAK;    // reset into topic mode
+                trans.ition();     // trasition back to home screen
             } else {
                 sock.et.emit('toOther', send.to);
-                edit.increment();  // increase row number to edit
-                send.mode = BLOCK; // block user till other responds
-                time.stopSend("");   // stop the clock from running anymore
+                edit.increment();            // increase row number to edit
+                send.mode = BLOCK;           // block user till other responds
+                textBar.changeAction(BLOCK); // show wait notice
+                time.stopSend("");           // stop the clock from running anymore
             }
         }
         send.empty = true; // it will be empty when it is responded to.
@@ -128,9 +117,9 @@ var send = { // dep: sock, trans, edit, textBar
             }
             sock.et.emit("breaking", textBar.entry.value);
         }else if(send.mode === CHAT){
-            if(send.empty){edit.onStart(); send.empty = false;}// account for nessisary transitions
-            edit.type({text: textBar.entry.value, row: 0});
-            sock.et.emit("chat", {text: textBar.entry.value, id: send.to});
+            if(send.empty){edit.onStart(); send.empty = false;} // account for nessisary transitions
+            edit.type({text: textBar.entry.value, row: 0});     // print on own screen
+            sock.et.emit("chat", {text: textBar.entry.value, id: send.to}); // send to other user
         }
         else if(send.mode === BLOCK){ // block more input from happening, leaving last sent message in box
             textBar.entry.value = textBar.entry.value.substring(0, textBar.entry.value.length -1);
@@ -140,11 +129,12 @@ var send = { // dep: sock, trans, edit, textBar
         sock.et.emit('post');         // Signal to the server that composition of topic is done
         time.stopSend(WAIT_TIME);     // in case this was called by passOn
         send.mode = BLOCK;            // block input till time to live is over
+        textBar.changeAction(BLOCK);  // display notice of block
         time.countDown(SEND_TIMER, function(){
             time.stopSend("");        // reset timer
-            textBar.entry.value = ""; // empty text
             send.empty = true;        // text is now empty
-            send.mode = BREAK;
+            send.mode = BREAK;        // set so topics can be made again
+            textBar.changeAction(BREAK); // display notice that topics can be made again
         });
     }
 }
@@ -189,17 +179,21 @@ var topic = { // dep: rows, sock, time, edit
 var sock = {  // dep: sockets.io, topic, trans, edit, send
     et: io(), // connect to server the page was served from
     init: function (){
-        sock.et.on('post', topic.post); // starts timer and stores user of topic
-        sock.et.on('topic', topic.ttl); // grab time to live topics: timed from the getgo
-        sock.et.on('chatInit', trans.gotBreak);
-        sock.et.on('toMe', edit.type);
-        sock.et.on('yourTurn', edit.myTurn);
-        sock.et.on('endChat', function(){
-            trans.ition(); // switch back to default appearence
-            send.mode = BREAK;
-        });
+        sock.et.on('post', topic.post);         // starts timer and stores user of topic
+        sock.et.on('topic', topic.ttl);         // grab time to live topics: timed from the getgo
+        sock.et.on('chatInit', trans.gotBreak); // someone wants to chat with us
+        sock.et.on('toMe', edit.type);          // recieve Real Time Text
+        sock.et.on('yourTurn', edit.myTurn);    // signals when it is this clients turn to type
+        sock.et.on('endChat', trans.ition);     // switch back to default appearence
     }
 }
+
+// info messages
+var INFO_HOME = "Pick a topic or create topic";
+var INFO_CHAT = "Chat: Pressing done while text box empty, ends chat";
+var INFO_TIMEOUT = "Actions auto-complete on timeout";
+var INFO_REJECTION = "This word will reject you from the conversation";
+var INFO_FILTER = "Words can be auto rejected by preferance with a filter";
 
 // informational header, this can change from screen to screen to indicate use information
 var info = {
@@ -249,10 +243,15 @@ var time = { // dep: document
         for(var i = 0; i < NUM_TIMERS; i++){time.rs.push(document.getElementById('timer' + i));}
     },
     from: function(whichRow, who){ // replaces time span elemement with perspective of user
-        time.rs[whichRow].style.visibility = "visible";
-        time.rs[whichRow].innerHTML = who;
-    },
+        time.rs[whichRow].style.visibility = "visible"; // make the element visible from within invisible button
+        time.rs[whichRow].innerHTML = who;              // which perspective its this
+    }, // Should probably be done with a seperate element but for the sake of simplicity this one is reused
 }
+
+// send text
+var SEND_TOPIC = "Make Topic ";
+var SEND_WAIT = "Wait ";
+var SEND_RESPONSE = "Done ";
 
 // Bottom of page text bar footer object
 var textBar = { // dep: document, send
@@ -264,24 +263,36 @@ var textBar = { // dep: document, send
         textBar.entry.oninput = send.input;      // block when not user's turn
         textBar.button.onclick = send.passOn;
     },
+    changeAction: function(mode){
+        if(mode === BREAK){
+            textBar.btnTxt.innerHTML = SEND_TOPIC;
+            textBar.entry.value = "";
+        } else if ( mode === CHAT ){
+            textBar.btnTxt.innerHTML = SEND_RESPONSE;
+            textBar.entry.value = "";
+        } else if ( mode === BLOCK){
+            textBar.btnTxt.innerHTML = SEND_WAIT;
+        }
+    }
 }
 
+var WAIT_MSG = "Waiting for topics...";
 // anything to do with the topic selection buttons or row data
 var rows = { // dep: document, trans
     button: [],
     dialog: [],
     init: function() { // actions for selection of topic
         for(var i = 0; i < NUM_ENTRIES; i++){                        // for every entry and
-            rows.dialog.push(document.getElementById("dialog" + i));
+            rows.dialog.push(document.getElementById("dialog" + i)); // store dialog elements
             rows.button.push(document.getElementById("button" + i)); // store a button element
             rows.button[i].onclick = trans.selBreak;                 // give the button its action
         }
     },
     reset: function() {
-        for(var i = 0; i < NUM_ENTRIES; i++){                     // for every entry and
-            if(i){rows.dialog[i].innerHTML = "";}
-            else{rows.dialog[i].innerHTML = WAIT_MSG;}
-            rows.button[i].style.visibility = "hidden";
+        for(var i = 0; i < NUM_ENTRIES; i++){           // for every entry and
+            if(i){rows.dialog[i].innerHTML = "";}       // set dialog to be empty
+            else{rows.dialog[i].innerHTML = WAIT_MSG;}  // set a default entry for dialog 0
+            rows.button[i].style.visibility = "hidden"; // hide the buttons
         }
     }
 }
