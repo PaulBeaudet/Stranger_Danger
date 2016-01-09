ab99e464b9c976fcdbd4b782106e39814056c869
paulBeaudet <inof8or@gmail.com>
2016-01-09 16:04:43 -0500

    try a better way to get the object.id

--- serve.js
+++ serve.js
@@ -24,11 +24,11 @@ var topicDB = {                                        // depends on mongo
         mongo.topic.count().exec(function(err, count){ // find out which topic this will be
             doc.index = count;                         // add unique count property to document
             doc.author = ID.email;                     // note the author of the topic
-            doc.save(function(err){                    // write new topic to database
+            doc.save(function(err, created){                    // write new topic to database
                 if(err){console.log(err + ' onCreate');}
                 else {
                     topicDB.temp.push(text);           // also add topic in temorary array
-                    userDB.temp[userNum].subIDs.push(doc.id); // add to this user subscriptions
+                    userDB.temp[userNum].subIDs.push(created.id); // add to this user subscriptions
                 }
             });                                        // TODO subscribe user to topic (return count ID of topic)
         });
